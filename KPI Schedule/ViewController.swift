d123e5cf2b5c2e8efdf354d36eeead2822f0a4ba
Vladimir <vovakovalev2016@icloud.com>
2023-01-24 15:56:20 +0300

    arrange the starting screen and add dark mode

--- KPI Schedule/ViewController.swift
+++ KPI Schedule/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var switcher: UISwitch!
+    @IBOutlet var backgroundView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        switcher.isOn = false
     }
 
 
+    @IBAction func switchMode(_ sender: UISwitch) {
+        if switcher.isOn {
+            backgroundView.backgroundColor = #colorLiteral(red: 0.2078431373, green: 0.3137254902, blue: 0.4392156863, alpha: 1)
+        }
+        else {
+            backgroundView.backgroundColor = #colorLiteral(red: 0.7764705882, green: 0.6745098039, blue: 0.5607843137, alpha: 1)
+        }
+    }
 }
 
